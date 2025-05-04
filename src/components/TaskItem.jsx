34c355e3500e97a2a6b9684dca4d0925ee0feb6a
GuanactolayRodrigo <42583662@fi.unju.edu.ar>
2025-05-04 19:36:07 -0300

    Agregado de div a TaskItem y cambio de <p> a <spam>

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,15 +1,16 @@
 const TaskItem = ({ tarea, cambiarEstadoTarea, eliminarTarea }) => {
     return (
-        <li>
-            <p style={{ textDecoration: tarea.completa ? "line-through" : "none" }}>
-                {tarea.nuevaTarea}
+        <div>
+            <li>
+                <span style={{ textDecoration: tarea.completa ? "line-through" : "none" }}>
+                    {tarea.nuevaTarea}
+                </span>
                 <button className="completar" onClick={() => cambiarEstadoTarea(tarea.id)}>
-                {tarea.completa ? "Desmarcar" : "Completa"}
+                        {tarea.completa ? "Desmarcar" : "Completa"}
                 </button>
                 <button className="borrar" onClick={() => eliminarTarea(tarea.id)}>Borrar</button>
-            </p>
-            
-        </li>
+            </li>
+        </div>
     );
 };
 
